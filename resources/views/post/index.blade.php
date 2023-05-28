4dba187575dfa8f1553f65904dce0d57019ad1f2
DaiNaka <wxfpk432@gmail.com>
2023-05-28 11:37:38 +0900

    :art: Add style for image

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
+
+    <style>
+        img {max-width: 100%; height: auto;}
+    </style>
+    
 </head>
 <body>
     <h1>{{config('app.name')}}</h1>
