cde40f3743cd40dfce9661156fc6f0978ef43f38
DaiNaka1207 <wxfpk432@gmail.com>
2023-12-26 02:13:42 +0900

    :art: Change design of view screen #7

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,18 +8,30 @@
 
     <style>
         img {max-width: 100%; height: auto;}
+        .l-container {margin: auto;max-width: 1280px; width: 80%; background-color: #bae6fd;}
+        .p-siteTitle::first-letter {text-transform: uppercase;}
+        .l-header {display: flex; align-items: center; height: 26px; margin-bottom: 5px;}
+        .updated_at {font-size: 0.85rem; margin-left: 15px; margin-bottom: 0;}
+        .p-button {height: max-content;}
+        .l-footer a {text-decoration: none; display: block; text-align: center}
+        .l-contents {background-color: #fff; border-radius: 10px; padding: 10px}
+        .l-footer {margin-top: 20px;}
     </style>
     
 </head>
-<body>
-    <h1>{{config('app.name')}}</h1>
-    <a href="{{route('post.edit', ['post' => $post->id])}}">編集</a>
-    <hr>
+<body class="l-container">
+    <h1 class="p-siteTitle">{{config('app.name')}}</h1>
+    <div class="l-header">
+        <button class="p-button" type="button" onclick="location.href='{{route('post.edit', ['post' => $post->id])}}'">編集</button>
+        <p class="updated_at">📝{{ $post->updated_at }}</p>
+    </div>
 
-    {!! $post->contents !!}
+    <div class="l-contents">
+        {!! $post->contents !!}
+    </div>
 
-    <hr>
-    <small><a href="https://portfolio.dainaka.live">&copy; 2023 DaiNaka</a></small>
-
+    <div class="l-footer">
+        <small><a href="https://portfolio.dainaka.live">&copy; 2023 DaiNaka</a></small>
+    </div>
 </body>
 </html>
