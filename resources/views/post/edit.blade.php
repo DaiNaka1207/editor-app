74589be68c0ead1d6a20bb10c87800d4ec805d47
DaiNaka1207 <wxfpk432@gmail.com>
2023-12-26 02:29:11 +0900

    :art: Change design of edit screen #7

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -7,21 +7,29 @@
     <title>{{config('app.name')}}</title>
 
     <style>
-        ul {list-style: none; padding-left: 0;}
+        .l-container {margin: auto;max-width: 1280px; width: 80%; background-color: #bae6fd;}
+        .p-siteTitle::first-letter {text-transform: uppercase;}
+        .l-footer a {text-decoration: none; display: block; text-align: center}
+        .l-footer {margin-top: 20px;}
+        .p-ul {list-style: none; padding-left: 0;}
+        .p-button {margin-bottom: 5px; margin-right: 10px;}
     </style>
 
     <x-head.tinymce-config />
 </head>
-<body>
-    <h1>{{config('app.name')}}</h1>
+<body class="l-container">
+    <h1 class="p-siteTitle">{{config('app.name')}}</h1>
 
     <form action="{{route('post.update', ['post' => $post->id])}}" method="POST">
         @csrf @method('PUT')
-        <ul>
-            <li><textarea id="editor" name="contents" cols="30" rows="10">{{$post->contents}}</textarea></li>
-            <li><button type="submit">送信</button></li>
-        </ul>
+        <button class="p-button p-button--send" type="submit">送信</button>
+        <button class="p-button p-button--cansel" type="button" onclick="location.href='{{config('app.url')}}'">取消</button>
+        <textarea id="editor" name="contents" cols="30" rows="10">{{$post->contents}}</textarea>
     </form>
+    
+    <div class="l-footer">
+        <small><a href="https://portfolio.dainaka.live">&copy; 2023 DaiNaka</a></small>
+    </div>
 
 </body>
 </html>
